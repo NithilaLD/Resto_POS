85aea94ae0ed68d5d3f121790783b0ad0a33d6ae
Dulan Nithila Liyanarachchi <nithila0411@gmail.com>
2024-06-16 12:01:42 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -14,7 +14,6 @@ function Login(){
   const navigate = useNavigate();
   const [password, setPassword] = useState("")
   const [divBgColor, setDivBgColor] = useState('#070005');
-  const [bColor, setbColor] = useState('#FDDC5C');
   const [round,setRound]=useState(0);
   const [spanBgColor1, setSpanBgColor1] = useState('#070005');
   const [spanBgColor2, setSpanBgColor2] = useState('#070005');
@@ -25,7 +24,6 @@ function Login(){
     const digit = num.toString();
     await setPassword((pre)=>pre + digit);
     setDivBgColor('#00008B');
-    setbColor('#AA6C39');
     setRound((prev)=>prev+1);
     if(round===0){setSpanBgColor1('white');setSpanBgColor2('#8B7500');setSpanBgColor3('#8B7500');setSpanBgColor4('#8B7500');}
     else if(round===1){setSpanBgColor1('white');setSpanBgColor2('white');setSpanBgColor3('#8B7500');setSpanBgColor4('#8B7500');}
